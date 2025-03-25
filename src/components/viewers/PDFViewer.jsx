b828a69471411d0cc605f091a34197f882757c8e
Get-analytics <team20tech@gmail.com>
2025-03-25 16:01:29 +0530

    Update PDFViewer.jsx

--- src/components/viewers/PDFViewer.jsx
+++ src/components/viewers/PDFViewer.jsx
@@ -418,7 +418,7 @@ import React, {
         <Worker workerUrl={pdfjsWorker}>
           <Viewer
             fileUrl={fileUrl}
-            defaultScale={isMobile ? 0.8 : 1.8}
+            defaultScale={device?.toLowerCase() === "mobile" ? 0.8 : 1.8}
             renderMode="canvas"
             onPageChange={handlePageChange}
             plugins={[]}
